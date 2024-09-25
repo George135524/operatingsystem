5a3af85faa3344a38281fc68a1720c3c0d835747
George135524 <jorgedejesusjimenez46@gmail.com>
2024-09-25 12:39:06 +0000

    I made the boot up for the operating system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std; 
+void startingUp()
+{
+cout<<"Setting up hardware" <<endl;
+  cout<< "preparing bootloader"<<endl;
+  cout<< "Setting up Bios"<<endl;
+  cout<<"preparing the kernel"<<endl<<".\n"<<".\n"<<".\n";
+
+  
+}
+
+
 int main() 
 {  
+startingUp();
+  cout<< "Operating system is ready to be used";
 
-cout << "Hello Jorge";
-cout << "hello albert";
-cout << "Hello World";
 };
