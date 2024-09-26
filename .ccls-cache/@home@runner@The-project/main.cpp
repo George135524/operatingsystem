758e7d684f5d1c63892d14721bdf51cf24602684
George135524 <jorgedejesusjimenez46@gmail.com>
2024-09-26 02:00:27 +0000

    Addition of the login feature

--- .ccls-cache/@home@runner@The-project/main.cpp
+++ .ccls-cache/@home@runner@The-project/main.cpp
@@ -8,11 +8,31 @@ cout<<"Setting up hardware" <<endl;
   cout<<"preparing the kernel"<<endl<<".\n"<<".\n"<<".\n";
 
 }
+void loginAuthentication()
+{
+cout<< "Enter the password"<<endl;
+string password;
+  
+   do{
+     cout<<"Enter the password"<<endl;
+     cin>> password;
+     if(password !="yes23"){
+     cout<< "access denied try again"<<endl; 
+     }
+     else
+     {
+       cout<<"Welcome back user"<<endl;
+     }
+   }while(password != "yes23");
+  
+}
 
 int main() 
 {  
   
 startingUp();
   cout<< "Operating system is ready to be used";
+loginAuthentication();
 
+  
 };
